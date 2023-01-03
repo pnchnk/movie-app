807c141820aa47bd0b9896fdec5696a8473b6b83
Nikita Panchenko <pnchnk.nikita@gmail.com>
2023-01-03 12:25:09 +0200

    beginning

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Button } from '@/components/UI/Button';
+export { default as LeftAside } from '@/components/LeftAside';
+export { default as RightAside } from '@/components/RightAside';
+export { default as Header } from '@/components/Header';
