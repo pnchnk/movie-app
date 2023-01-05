2c6598b0314b924ddc5ad928fbe84eac8823863c
Nikita Panchenko <pnchnk.nikita@gmail.com>
2023-01-05 16:35:08 +0200

    right aside header

--- src/components/RightAside/index.tsx
+++ src/components/RightAside/index.tsx
@@ -2,8 +2,27 @@ import React, { FC } from 'react';
 
 import './styles.scss';
 
+import {ReactComponent as NotificationIcon} from "@/assets/icons/notification.svg";
+
+import {ReactComponent as ArrowIcon} from "@/assets/icons/next.svg";
+
 const RightAside: FC = () => {
-  return <aside className="right-aside">3</aside>;
+  return (
+    <aside className="right-aside">
+      <div className="right-aside-header">
+          <div className="right-aside-header__notification">
+            <NotificationIcon/>
+          </div>
+          <div className="right-aside-header__profile">
+            <span>Samantha</span>
+            <ArrowIcon className='right-aside-header-arrow'/>
+            <div className="right-aside-header__profile-picture">
+              <img src={require("../../assets/images/profile.jpg")} alt="" />
+            </div>
+          </div>
+      </div>
+    </aside>
+    );
 };
 
 export default RightAside;
